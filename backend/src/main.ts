c2d4d7299b33d218021c85b0a554cfe59450b66d
ababoum <ababou237@hotmail.com>
2022-10-03 09:06:11 +0200

    add Procfile for Heroku

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,14 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
+	let port: number = + process.env.PORT;
+
+	if (port == null || port == 0) {
+		port = 3000;
+	}
 
 	app.use(bodyParser.json());
-	await app.listen(3000);
+	await app.listen(process.env.PORT);
 }
 
 
