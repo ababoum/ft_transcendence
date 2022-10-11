66f750fd9bf4807b684e421df81ed4742c6af6b5
ellacroix <eliot.lacroix@essec.edu>
2022-10-11 11:14:06 +0200

    Methods for database management of chatroom

--- backend/src/chatroom/chatroom.controller.ts
+++ backend/src/chatroom/chatroom.controller.ts
@@ -11,7 +11,9 @@ import {
 } from '@nestjs/common';
 import { ChatroomService } from './chatroom.service';
 import { ChatRoom as ChatRoomModel } from '@prisma/client';
-import { User } from '@prisma/client';
+import { User as UserModel } from '@prisma/client';
+import { chatRoomType } from '@prisma/client';
+
 
 @Controller('chatroom')
 export class ChatroomController {
@@ -25,31 +27,139 @@ export class ChatroomController {
 
  	@Post('create')
 	async createChatRoom(
-		@Body() ChatRoomData: { name: string, owneremail: string },
+		@Body() ChatRoomData: { name: string, ownerlogin: string, mode?: chatRoomType},
 	): Promise<ChatRoomModel> {
-		const { name, owneremail } = ChatRoomData;
+		const { name, ownerlogin, mode } = ChatRoomData;
 		return this.ChatroomService.createChatRoom({
 			name,
 			owner: {
-				connect: { email: owneremail }
+				connect: { login: ownerlogin }
+			},
+			mode,
+			participants: {
+				connect: {login: ownerlogin }
+			},
+			admin: {
+				connect: {login: ownerlogin }
 			}
 		});
 	}
 
-/* 	@Post('create')
-	async createChatRoom(
-		@Body() ChatRoomData: { name: string; owneremail: string},
-	): Promise<ChatRoomModel> {
-		const { name, owneremail,  } = ChatRoomData;
-		return this.ChatroomService.createChatRoom({
-			name,
-			owner: {
-				connect: { email: owneremail},
-			},
-			participants: undefined,
-			banList: undefined,
-			muteList: undefined
+// PARTICIPANTS //
+	@Get('participants')
+	async participants(@Body() ChatRoomData: { chatroomid: number } ) {
+		const {chatroomid} = ChatRoomData;
+		return this.ChatroomService.participantsByChatRoom(chatroomid);
+	}
+
+	@Post('invite')
+	async invite(
+		@Body() ChatRoomData: { chatroomid: number, userlogin: string },
+	) {
+		const { chatroomid, userlogin } = ChatRoomData;
+		return this.ChatroomService.inviteUser({
+			chatroomid,
+			userlogin
 		});
-	} */
+	}
+
+	@Post('leave')
+	async leave(
+		@Body() ChatRoomData: { chatroomid: number, userlogin: string },
+	): Promise<ChatRoomModel> {
+		const { chatroomid, userlogin } = ChatRoomData;
+		return this.ChatroomService.leaveChatRoom({
+			chatroomid,
+			userlogin
+		});
+	}
+
+	// MUTE //
+		@Get('adminList')
+		async adminList(@Body() ChatRoomData: { chatroomid: number } ) {
+			const {chatroomid} = ChatRoomData;
+			return this.ChatroomService.adminListByChatRoom(chatroomid);
+		}
+
+		@Post('admin')
+		async adminUser(
+			@Body() ChatRoomData: { chatroomid: number, userlogin: string },
+		) {
+			const { chatroomid, userlogin } = ChatRoomData;
+			return this.ChatroomService.adminUser({
+				chatroomid,
+				userlogin
+			});
+		}
+
+		@Post('unadmin')
+		async unadminUser(
+			@Body() ChatRoomData: { chatroomid: number, userlogin: string },
+		): Promise<ChatRoomModel> {
+			const { chatroomid, userlogin } = ChatRoomData;
+			return this.ChatroomService.unadminUser({
+				chatroomid,
+				userlogin
+			});
+		}
+
+// MUTE //
+	@Get('muteList')
+	async muteList(@Body() ChatRoomData: { chatroomid: number } ) {
+		const {chatroomid} = ChatRoomData;
+		return this.ChatroomService.muteListByChatRoom(chatroomid);
+	}
+
+	@Post('mute')
+	async muteUser(
+		@Body() ChatRoomData: { chatroomid: number, userlogin: string },
+	) {
+		const { chatroomid, userlogin } = ChatRoomData;
+		return this.ChatroomService.muteUser({
+			chatroomid,
+			userlogin
+		});
+	}
+
+	@Post('unmute')
+	async unmuteUser(
+		@Body() ChatRoomData: { chatroomid: number, userlogin: string },
+	): Promise<ChatRoomModel> {
+		const { chatroomid, userlogin } = ChatRoomData;
+		return this.ChatroomService.unmuteUser({
+			chatroomid,
+			userlogin
+		});
+	}
+
+// BAN //
+	@Get('banList')
+	async banList(@Body() ChatRoomData: { chatroomid: number } ) {
+		const {chatroomid} = ChatRoomData;
+		return this.ChatroomService.banListByChatRoom(chatroomid);
+	}
+
+	@Post('ban')
+	async banUser(
+		@Body() ChatRoomData: { chatroomid: number, userlogin: string },
+	) {
+		const { chatroomid, userlogin } = ChatRoomData;
+		this.ChatroomService.banUser({
+			chatroomid,
+			userlogin
+		});
+		return this.ChatroomService.banListByChatRoom(chatroomid);
+	}
+
+	@Post('unban')
+	async unbanUser(
+		@Body() ChatRoomData: { chatroomid: number, userlogin: string },
+	): Promise<ChatRoomModel> {
+		const { chatroomid, userlogin } = ChatRoomData;
+		return this.ChatroomService.unbanUser({
+			chatroomid,
+			userlogin
+		});
+	}
 
 }
--- backend/src/chatroom/chatroom.service.ts
+++ backend/src/chatroom/chatroom.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { ChatRoom, Prisma } from '@prisma/client';
+import { ChatRoom, User, Prisma } from '@prisma/client';
 
 @Injectable()
 export class ChatroomService {
@@ -13,7 +13,7 @@ export class ChatroomService {
 		  where: userWhereUniqueInput,
 		});
 	  }
-	
+
 	  async chatRooms(params: {
 		skip?: number;
 		take?: number;
@@ -31,23 +31,128 @@ export class ChatroomService {
 		});
 	  }
 
-	async createChatRoom(data): Promise<ChatRoom> {
+	async createChatRoom(data: Prisma.ChatRoomCreateInput): Promise<ChatRoom> {
 		return this.prisma.chatRoom.create({data});
 	}
 
+// PARTICIPANTS //
+	async participantsByChatRoom(chatroomid: number) {
+		return this.prisma.user.findMany({
+			where: { chatRoomJoined: { some: { id: chatroomid } } },
+			select: { id: true, login: true },
+		})
+	}
 
-/* 	async createChatRoom(data): Promise<ChatRoom> {
-		return this.prisma.chatRoom.create({
-		  data: {
-			name:"Plop",
-			owner: {
-				connect: {
-					email: "aaa"
-				}
-			}
-		  }
+	async inviteUser(params: {chatroomid: number; userlogin: string;}): Promise<ChatRoom> {
+		const { chatroomid, userlogin } = params;
+		return this.prisma.chatRoom.update({
+			where: { id: chatroomid },
+			data: {
+				participants: { connect: { login: userlogin } },
+			},
 		});
 	}
- */
+
+	async leaveChatRoom(params: {chatroomid: number; userlogin: string;}): Promise<ChatRoom> {
+		const { chatroomid, userlogin } = params;
+		// if (this.prisma.chatRoom.ownerlogin = userlogin) {
+		// 	return this.prisma.chatRoom.update({
+		// 		where: { id: chatroomid },
+		// 		data: {
+		// 			owner: { connect : { id: 1 } },
+		// 			admin: { disconnect : { login: userlogin } },
+		// 			participants: { disconnect : { login: userlogin } },
+		// 		},
+		// 	});
+		// }
+			return this.prisma.chatRoom.update({
+				where: { id: chatroomid },
+				data: {
+					admin: { disconnect : { login: userlogin } },
+					participants: { disconnect : { login: userlogin } },
+				},
+			});
+	}
+
+// ADMIN //
+	async adminListByChatRoom(chatroomid: number): Promise<User[]> {
+		return this.prisma.user.findMany({
+			where: { chatRoomAdmined: { some: { id: chatroomid } } }
+		})
+	}
+
+	async adminUser(params: {chatroomid: number; userlogin: string;}): Promise<ChatRoom> {
+		const { chatroomid, userlogin } = params;
+		return this.prisma.chatRoom.update({
+			where: { id: chatroomid },
+			data: {
+				admin: { connect: { login: userlogin } },
+			},
+		});
+	}
+
+	async unadminUser(params: {chatroomid: number; userlogin: string;}): Promise<ChatRoom> {
+		const { chatroomid, userlogin } = params;
+		return this.prisma.chatRoom.update({
+			where: { id: chatroomid },
+			data: {
+				admin: { disconnect : { login: userlogin } },
+			},
+		});
+	}
+
+// MUTE //
+	async muteListByChatRoom(chatroomid: number): Promise<User[]> {
+		return this.prisma.user.findMany({
+			where: { chatRoomMuted: { some: { id: chatroomid } } }
+		})
+	}
+
+	async muteUser(params: {chatroomid: number; userlogin: string;}): Promise<ChatRoom> {
+		const { chatroomid, userlogin } = params;
+		return this.prisma.chatRoom.update({
+			where: { id: chatroomid },
+			data: {
+				muteList: { connect: { login: userlogin } },
+			},
+		});
+	}
+
+	async unmuteUser(params: {chatroomid: number; userlogin: string;}): Promise<ChatRoom> {
+		const { chatroomid, userlogin } = params;
+		return this.prisma.chatRoom.update({
+			where: { id: chatroomid },
+			data: {
+				muteList: { disconnect : { login: userlogin } },
+			},
+		});
+	}
+
+// BAN //
+	async banListByChatRoom(chatroomid: number): Promise<User[]> {
+		return this.prisma.user.findMany({
+			where: { chatRoomBanned: { some: { id: chatroomid } } }
+		})
+	}
+
+	async banUser(params: {chatroomid: number; userlogin: string;}): Promise<ChatRoom> {
+		const { chatroomid, userlogin } = params;
+		return this.prisma.chatRoom.update({
+			where: { id: chatroomid },
+			data: {
+				banList: { connect: { login: userlogin } },
+			},
+		});
+	}
+
+	async unbanUser(params: {chatroomid: number; userlogin: string;}): Promise<ChatRoom> {
+		const { chatroomid, userlogin } = params;
+		return this.prisma.chatRoom.update({
+			where: { id: chatroomid },
+			data: {
+				banList: { disconnect : { login: userlogin } },
+			},
+		});
+	}
 
 }
