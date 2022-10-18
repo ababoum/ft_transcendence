d4194ef8fe1d8a82b2420fdd7c541ceca8b0d6e6
ellacroix <eliot.lacroix@essec.edu>
2022-10-18 16:28:17 +0200

    Start of ChatRoom sockets

--- frontend/src/routes/ChatRoom.svelte
+++ frontend/src/routes/ChatRoom.svelte
@@ -12,6 +12,12 @@
 
 	let profile = undefined;
 
+	let chatrooms = [{id: 1, name: "Chatroom1"}, {id: 2, name: "Chatroom2"}]
+
+	let messages = [{nickname: "User1", content: "Hey !"}, 
+					{nickname: "User2", content: "How are you ?"}, 
+					{nickname: "User3", content: "Frontend sucks."}]
+
 	onDestroy(() => {
 	})
 </script>
@@ -31,65 +37,53 @@
               <div class="col-md-6 col-lg-5 col-xl-4 mb-4 mb-md-0">
 
                 <div class="p-3">
-
-                  <div class="input-group rounded mb-3">
-                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
-                      aria-describedby="search-addon" />
-                  </div>
-
+					
+					
                   <div data-mdb-perfect-scrollbar="true" style="position: relative; height: 400px">
                     <ul class="list-unstyled mb-0">
+					{#each chatrooms as chatroom}
                       <li class="p-2 border-bottom">
-                        <a href="#!" class="d-flex justify-content-between">
-                          <div class="d-flex flex-row">
-                            <div>
-                              <span class="badge bg-success badge-dot"></span>
-                            </div>
-                            <div class="pt-1">
-                              <p class="fw-bold mb-0">ChatRoom1</p>
-                              <p class="small text-muted">Hello, Are you there?</p>
-                            </div>
-                          </div>
-                          <div class="pt-1">
-                            <p class="small text-muted mb-1">Just now</p>
-                          </div>
-                        </a>
+                        <div class="pt-1">
+                          <p>{chatroom.name} <button>Enter</button> </p>
+                        </div>
                       </li>
+					{/each}
                     </ul>
                   </div>
 
+
                 </div>
 
               </div>
 
-              <div class="col-md-6 col-lg-7 col-xl-8">
+		          <div class="col-md-6 col-lg-7 col-xl-8">
 
-                <div class="pt-3 pe-3" data-mdb-perfect-scrollbar="true"
-                  style="position: relative; height: 400px">
+		            <div class="pt-3 pe-3" data-mdb-perfect-scrollbar="true"
+		              style="position: relative; height: 400px">
+					  
+					  {#each messages as message}
 
-                  <div class="d-flex flex-row justify-content-start">
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"
-                      alt="avatar 1" style="width: 45px; height: 100%;">
-                    <div>
-                      <p class="small p-2 ms-3 mb-1 rounded-3" style="background-color: #f5f6f7;">Lorem ipsum
-                        dolor
-                        sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore
-                        magna aliqua.</p>
-                      <p class="small ms-3 mb-3 rounded-3 text-muted float-end">12:00 PM | Aug 13</p>
-                    </div>
-                  </div>
+		              <div class="d-flex flex-row justify-content-start">
+		                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"
+		                  alt="avatar 1" style="width: 45px; height: 100%;">
+		                <div>
+		                  <p class="small p-2 ms-3 mb-1 rounded-3" style="background-color: #f5f6f7;">{message.nickname}: {message.content}</p>
+		                  <p class="small ms-3 mb-3 rounded-3 text-muted">12:00 PM | Aug 13</p>
+		                </div>
+		              </div>
 
-                </div>
+					  {/each}
 
-                <div class="text-muted d-flex justify-content-start align-items-center pe-3 pt-3 mt-2">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"
-                    alt="avatar 3" style="width: 40px; height: 100%;">
-                  <input type="text" class="form-control form-control-lg" id="exampleFormControlInput2"
-                    placeholder="Type message">
-                </div>
+		            </div>
 
-              </div>
+		            <div class="text-muted d-flex justify-content-start align-items-center pe-3 pt-3 mt-2">
+		              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"
+		                alt="avatar 3" style="width: 40px; height: 100%;">
+		              <input type="text" class="form-control form-control-lg" id="exampleFormControlInput2"
+		                placeholder="Type message">
+		            </div>
+
+		          </div>
             </div>
 
           </div>
