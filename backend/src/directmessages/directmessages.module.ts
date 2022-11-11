ebdf394bc194943dae71aaf9b5b275f971c197d2
ellacroix <eliot.lacroix@essec.edu>
2022-11-11 10:37:47 +0100

    Small fix

--- backend/src/directmessages/directmessages.module.ts
+++ backend/src/directmessages/directmessages.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { DirectmessagesController } from './directmessages.controller';
 import { DirectmessagesGateway } from './directmessages.gateway';
 import { DirectmessagesService } from './directmessages.service';
@@ -6,6 +7,7 @@ import { DirectmessagesService } from './directmessages.service';
 @Module({
   controllers: [DirectmessagesController],
   providers: [	DirectmessagesService,
-				DirectmessagesGateway]
+				DirectmessagesGateway,
+				PrismaService]
 })
 export class DirectmessagesModule {}
